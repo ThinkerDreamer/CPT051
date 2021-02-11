72f1dec9852461c27bc41fa5fa0d312f6ba54574
Noah Adkin <50452002+NAdkin@users.noreply.github.com>
2021-02-11 10:33:26 -0500

    Create noelle_flowchart_example2.py

---
+++ noelle_flowchart_example2.py
@@ -0,0 +1,19 @@
+#start
+
+#Display "Enter a number"
+print("Enter a number:")
+#Read N
+n = int(input())
+
+#if N< 0
+if n < 0:
+    print("Negative number")
+elif n == 0:
+    print("Zero")
+else:          
+    print("Positive number")
+#Write negative number
+
+#write "positive number"
+
+#end

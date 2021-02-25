bc48527a5e4255beef5ff40fc72316051d4e828c
N Adkin <50452002+NAdkin@users.noreply.github.com>
2021-02-25 10:45:39 -0500

    sql file

--- noelle_object_1.py
+++ noelle_object_1.py
@@ -7,3 +7,49 @@
 #lesson_title
 #pay_amount
 #students_enrolled
+
+
+def take_input():
+    print("Enter your selection here")
+    selection = input()
+    selection = selection.upper()
+    selection = selection[0]
+    return selection
+
+class_types = ['social studies', 'maths' , 'sciences']
+
+
+class Course():
+    def __init__(self, lesson_title):
+        self.lesson_title = lesson_title
+        
+        
+    def set_class_type(self):
+        print("What is the class_type?")
+
+        count = 0
+        for i in class_types:
+            print(str(count) + ' ' + i)
+            count += 1
+
+        my_type = input()
+        my_type = my_type[0]
+        my_type = int(my_type)
+        
+        self.class_type = class_types[my_type]
+        return self.class_type
+        
+    
+
+
+#main
+print("What is the class name?")
+my_lesson_title = input()
+
+my_class = Course(my_lesson_title)
+
+my_local_display = my_class.set_class_type()
+
+print("local display: " + my_local_display)
+print(my_class.lesson_title)
+print(my_class.class_type)
---
+++ noelle_sql_pthon_demo.py
@@ -0,0 +1,31 @@
+#https://www.w3schools.com/sql/sql_insert.asp
+#https://www.w3schools.com/python/python_mysql_select.asp
+import mysql.connector
+
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="discord"
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO firstnames(name) VALUES (%s)"
+val = ("Taj", )
+
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
+
+##selects
+
+mycursor.execute("SELECT * FROM firstnames")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)

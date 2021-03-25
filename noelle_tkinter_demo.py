5fbfdd77d400792363a30c1ac6e64888a1e3af17
N Adkin <50452002+NAdkin@users.noreply.github.com>
2021-03-25 14:26:14 -0400

    Update noelle_tkinter_demo.py

--- noelle_tkinter_demo.py
+++ noelle_tkinter_demo.py
@@ -7,7 +7,12 @@ now = datetime.now()
 #text_box.insert("1.0", "Hello")
 
 def handle_click_showall(event):
+    print("showall")
     show_all(mycursor)
+
+def handle_click_submit():
+    print("Submit")
+    insert_student(mycursor)
 
 def show_all(mycursor):
 
@@ -18,9 +23,26 @@ def show_all(mycursor):
     for x in myresult:
         count += 1
         #print(x)
+        y = str(x)
         ind = str(count)
         ind = "0." + ind
-        text_box.insert(ind, x)
+        text_box.insert(ind, y + "\n")
+
+def insert_student(mycursor):
+
+    name = entry.get()
+    myname = name
+
+    sql = "INSERT INTO PATIENT(first_name, last_name, date_added, doc_id) VALUES (%s, %s, %s, %s)"
+    val = (myname, "Lastnameson", now, "1" )
+
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    print(mycursor.rowcount, "record inserted.")
+
+
 
 #connection
 mydb = mysql.connector.connect(
@@ -35,7 +57,7 @@ mycursor = mydb.cursor()
 
 window = tk.Tk()
 
-
+#b1
 button = tk.Button(
     text="Show All Students(button)",
     width=25,
@@ -46,25 +68,25 @@ button = tk.Button(
 button.pack()
 
 button.bind("<Button-1>", handle_click_showall)
-
+#end b1
+#Tbox
 text_box = tk.Text(height = 26, width = 200)
 text_box.pack()
-
+#endofTBOX
 
 
 entry = tk.Entry()
 entry.pack()
 
-name = entry.get()
 
-button2 = tk.Button(
-    text="Submit",
-    width=25,
-    height=5,
-    bg="blue",
-    fg="yellow",
-)
+
+button2 = tk.Button(text="Submit", command=handle_click_submit)
+
+
 button2.pack()
+
+
+
 
 window.mainloop()
 
